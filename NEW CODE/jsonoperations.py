b9725ec5c0839a34f8681150b97e151423b562dd
KID0MY <lukazbujes@gmail.com>
2023-05-02 22:33:01 -0300

    A

--- NEW CODE/jsonoperations.py
+++ NEW CODE/jsonoperations.py
@@ -2,3 +2,6 @@ class JsonOperations:
     # Json Operations(Working on it)
     def __init__(self,): #initialize Json Stuff
         print()
+
+    def CheckJson(self): #get old data + new data
+        print()
